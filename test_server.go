1892f7f58ccd5b4165818cbede689bc5c97b898f
zero_mind <chem_tomcat@mail.ru>
2019-10-07 16:25:02 +0300

    First workingcommit

--- test_server.go
+++ test_server.go
@@ -1,175 +1,81 @@
 package main
 
 import (
-	"flag"
+	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-	"strconv"
 	"strings"
-	"time"
 
-	"golang.org/x/net/websocket"
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
 )
 
 type Message struct {
-	Text string `json:"text"`
+	Version string          `json:"jsonrpc"`
+	Method  string          `json:"method,omitempty"`
+	Params  json.RawMessage `json:"params,omitempty"`
+	Error   json.RawMessage `json:"error,omitempty"`
+	Result  json.RawMessage `json:"result,omitempty"`
+	ID      int64           `json:"id,omitempty"`
 }
 
-type hub struct {
-	clients          map[string]*websocket.Conn
-	addClientChan    chan *websocket.Conn
-	removeClientChan chan *websocket.Conn
-	broadcastChan    chan Message
-}
-
-var (
-	port = flag.String("port", "1234", "port used for ws connection")
-)
-
-func main() {
-	flag.Parse()
-	log.Fatal(server(*port))
-}
-
-// server creates a websocket server at port <port> and registers the sole handler
-func server(port string) error {
-	h := newHub()
-	mux := http.NewServeMux()
-	mux.Handle("/ws", websocket.Handler(func(ws *websocket.Conn) {
-		handler(ws, h)
-	}))
-	mux.Handle("/", http.StripPrefix("/", noDirListing(http.FileServer(http.Dir("./static")))))
-
-	s := http.Server{Addr: ":" + port, Handler: mux}
-	return s.ListenAndServe()
-}
-
-// handler registers a new chat client conn;
-// It runs the hub, adds the client to the connection pool
-// and broadcasts received message
-func handler(ws *websocket.Conn, h *hub) {
-	go h.run()
-
-	h.addClientChan <- ws
-
-	for {
-		var m Message
-		// /var s string
-		//err := websocket.Message.Receive(ws, &s)
-		err := websocket.JSON.Receive(ws, &m)
-		if err != nil {
-
-			h.broadcastChan <- Message{err.Error()}
-			h.removeClient(ws)
-			return
-		}
-		fmt.Println("received: " + m.Text)
-
-		h.broadcastChan <- m
-	}
-}
-
-// newHub returns a new hub object
-func newHub() *hub {
-	return &hub{
-		clients:          make(map[string]*websocket.Conn),
-		addClientChan:    make(chan *websocket.Conn),
-		removeClientChan: make(chan *websocket.Conn),
-		broadcastChan:    make(chan Message),
-	}
-}
-
-// run receives from the hub channels and calls the appropriate hub method
-func (h *hub) run() {
-	for {
-		select {
-		case conn := <-h.addClientChan:
-			h.addClient(conn)
-		case conn := <-h.removeClientChan:
-			h.removeClient(conn)
-		case m := <-h.broadcastChan:
-			h.broadcastMessage(m)
-		}
-	}
-}
-
-// removeClient removes a conn from the pool
-func (h *hub) removeClient(conn *websocket.Conn) {
-	delete(h.clients, conn.LocalAddr().String())
-	fmt.Println("remove client")
-}
-
-// addClient adds a conn to the pool
-func (h *hub) addClient(conn *websocket.Conn) {
-	var s = conn.RemoteAddr().String() + strconv.FormatInt(time.Now().UnixNano(), 10)
-	h.clients[s] = conn
-	fmt.Println("add client " + s)
-}
-
-// broadcastMessage sends a message to all client conns in the pool
-func (h *hub) broadcastMessage(m Message) {
-	for _, conn := range h.clients {
-		err := websocket.JSON.Send(conn, m)
-
-		if err != nil {
-			fmt.Println("Error broadcasting message: ", err) // при некоторых типах ошибок удаляем клиента
-		} else {
-			fmt.Println("send to client")
-		}
-	}
-}
-
-func noDirListing(h http.Handler) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if strings.HasSuffix(r.URL.Path, "/") || r.URL.Path == "" {
-			http.NotFound(w, r)
-			return
-		}
-		h.ServeHTTP(w, r)
-	})
-
-}
-
-/*
-func main() {
-	http.Handle("/ws", websocket.Handler(Echo))
-	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./static"))))
-
-	if err := http.ListenAndServe(":1234", nil); err != nil {
-		log.Fatal("ListenAndServe:", err)
-	}
-}
-
-
-func main() {
-	//database.InitDB()
-	//defer database.MainDB.Close()
-
-	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./static"))))
-	http.HandleFunc("/login", loginHandler)
-
-	router := mux.NewRouter()
-
-	router.HandleFunc("/device", controllers.Device).Methods("POST")
-	router.HandleFunc("/ui", controllers.UI).Methods("POST")
-
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./HTML/")))
-
-	router.Use(app.AuthenticationMidlleware) //attach auth middleware
-
-	port := "80" //localhost 8000
-
-	fmt.Println(port)
-
-	go app.CheckAlarmsRoutine()
-
-	err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, _, _, err := ws.UpgradeHTTP(r, w)
 	if err != nil {
-		fmt.Print(err)
+		// handle error
 	}
+	go func() {
+		defer conn.Close()
+
+		for {
+			msg, op, err := wsutil.ReadClientData(conn)
+			fmt.Println("Rx <-", string(msg))
+			if err != nil {
+				fmt.Println("Read error")
+				return
+			}
+			var m Message
+			err = json.Unmarshal(msg, &m)
+			if err != nil {
+				fmt.Println("Parse error")
+				continue
+			}
+			s := msg
+			if m.Method == "testmethod" {
+				m.Method = ""
+				m.Result = []byte(`{"code": 1}`)
+				m.Params = []byte("{}")
+				s, err = json.Marshal(m)
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+
+			if m.Method == "ping" {
+				m.Method = "pong"
+				m.Params = nil
+				m.Result = nil
+				s, err = json.Marshal(m)
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+			fmt.Println("Tx ->", string(s))
+
+			err = wsutil.WriteServerMessage(conn, op, s)
+			if err != nil {
+				// handle error
+			}
+		}
+	}()
 }
 
+func main() {
+	http.Handle("/", noDirListing(http.StripPrefix("/", http.FileServer(http.Dir("")))))
+	http.HandleFunc("/ws", wsHandler)
+
+	http.ListenAndServe(":1234", nil)
+}
 func noDirListing(h http.Handler) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if strings.HasSuffix(r.URL.Path, "/") || r.URL.Path == "" {
@@ -178,5 +84,4 @@ func noDirListing(h http.Handler) http.HandlerFunc {
 		}
 		h.ServeHTTP(w, r)
 	})
-}*/
-
+}
--- wsmanager.js
+++ wsmanager.js
@@ -1,23 +1,67 @@
 
+
+
 var wsmanager = {
   'version': 1,
-  'configuration': {'port': 81, 'heartbeatinterval': 500},
-  'init': function() {
-    console.log('wsmanager v' + wsmanager.version + 'initialized');
-    wsmanager._queue = [];
-    wsmanager._currentTxPacket = null;
-    wsmanager._currentTxPacketID = 0;
-    wsmanager._lastHandledRxPacketID = -1;
+  'configuration':
+      {'url': '', 'heartBeatInterval': 500, 'sendWorkerInterval': 100},
+  'init': function(url) {
+    wsmanager.configuration.url = url;
+    console.log('wsmanager v' + wsmanager.version + ' initialized');
+    wsmanager._txQueue = [];  // {packet with ID, callback handler, repeat
+                              // interval, time for next repeat, time to delete}
+    wsmanager._rxQueue = [];  //
     wsmanager._state = 'offline';
     wsmanager._connect();
     wsmanager._statusChangedCallback = null;
-    wsmanager._onMessageCallback = null;
     wsmanager._heartbeatTimer = null;
-    wsmanager._reinitHeartbeatTimer();
+    wsmanager._initHeartbeatTimer();
     wsmanager._rxCallback = null;
+    wsmanager._nextFreeTxID = 1;
+    wsmanager._sendWorkerTimer = setInterval(
+        wsmanager._sendWorkerFunc, wsmanager.configuration.sendWorkerInterval);
+  },
+  '_sendWorkerFunc': function() {
+    for (let i = 0; i < wsmanager._txQueue.length; i++) {
+      let req = wsmanager._txQueue[i];
+      if (req.timeout > 0 &&
+          req.todelete >= wsmanager.configuration.sendWorkerInterval)
+        req.todelete -= wsmanager.configuration.sendWorkerInterval;
+      else {
+        if (typeof (req.callback) == 'function') {
+          // console.log(req);
+          req.callback('{\'error\':\'timeout\'}');
+        }
+        wsmanager._txQueue.splice(i, 1);
+        console.log('wsmanager: delete element from txQueue');
+        continue;
+      }
+      if (req.torepeat >= wsmanager.configuration.sendWorkerInterval)
+        req.torepeat -= wsmanager.configuration.sendWorkerInterval;
+      else {
+        if (wsmanager._state == 'online') {
+          console.log('wsmanager: try send...');
+          wsmanager._sock.send(req.packet);
+          req.torepeat = req.repeat;
+          wsmanager._stopHeartbeatTimer();
+          wsmanager._initHeartbeatTimer();
+        }
+      }
+    }
+
+    for (let i = 0; i < wsmanager._rxQueue.length; i++) {
+      let req = wsmanager._rxQueue[i];
+      if (req.todelete >= wsmanager.configuration.sendWorkerInterval)
+        req.todelete -= wsmanager.configuration.sendWorkerInterval;
+      else {
+        wsmanager._rxQueue.splice(i, 1);
+        console.log('wsmanager: delete element from rxQueue');
+        continue;
+      }
+    }
   },
   'setRxCallback': function(func) {
-    wsmanager.rxCallback = func;
+    wsmanager._rxCallback = func;
   },
   'setConnectionStateChangedCallback': function(func) {
     wsmanager._statusChangedCallback = func;
@@ -28,49 +72,89 @@ var wsmanager = {
   '_connect': function() {
     console.log('wsmanager: try connect...');
     wsmanager._sock = null;
-    wsmanager._sock = new WebSocket(wsmanager.configuration['port']);
-    wsmanager._sock.onopen = wsmanager._onwsopen;
-    wsmanager._sock.onclose = wsmanager._onwsclose;
-    wsmanager._sock.onmessage = wsmanager._rxHandler;
-  },
-  'tx': function(method, data, callback) {
-    let msg = {'jsonrpc': '2.0', 'method': method, 'params': data};
-    wsmanager._queue.push(JSON.stringify(msg));
-    if (wsmanager._currentTxPacket === null) {
-      wsmanager._prepareNextMsg();
-      wsmanager._trySend();
+    try {
+      wsmanager._sock = new WebSocket(wsmanager.configuration.url);
+      wsmanager._sock.onopen = wsmanager._onwsopen;
+      wsmanager._sock.onclose = wsmanager._onwsclose;
+      wsmanager._sock.onmessage = wsmanager._rxHandler;
+    } catch {
+      wsmanager._connect();
     }
   },
-  '_prepareNextMsg': function() {
-    if (wsmanager._queue.length > 0) {
-      wsmanager._currentTxPacket = wsmanager._queue.shift();
-      wsmanager._currentTxPacketID++;
-      wsmanager._currentTxPacket['id'] = wsmanager._currentTxPacketID;
-    }
+  'tx': function(method, data, timeout, callback) {
+    let id = wsmanager._nextFreeTxID++;
+    let msg = {'jsonrpc': '2.0', 'method': method, 'params': data, 'id': id};
+    wsmanager._txQueue.push({
+      'packet': JSON.stringify(msg),
+      'id': id,
+      'repeat': wsmanager.configuration.heartBeatInterval,
+      'torepeat': 0,
+      'timeout': timeout,
+      'todelete': timeout,
+      'callback': callback
+    });
   },
-  '_trySend': function() {
-    wsmanager._sock.send(wsmanager._currentTxPacket);
-  },
+
   '_rxHandler': function(e) {
     wsmanager._stopHeartbeatTimer();
     wsmanager._initHeartbeatTimer();
     // JSON parse, compare id with currentTx packet ID
     let packet = null;
     try {
-      packet = JSON.parse(raw);
+      packet = JSON.parse(e.data);
     } catch (err) {
-      console.log('wsmanager: error parsing rx package');
+      console.log('wsmanager: error parsing rx package:');
+      console.log(e);
       return;
     }
-    if (Number(packet.id) == Number(wsmanager._currentTxPacketID)) {
-      wsmanager._prepareNextMsg();
-      wsmanager._trySend();
-    }
-    if (packet.method !== undefined &&
-        wsmanager._lastHandledRxPacketID != Number(packet.id)) {
-      if (wsmanager._rxCallback !== null) wsmanager._rxCallback(packet);
-      wsmanager._lastHandledRxPacketID = Number(packet.id);
+
+
+
+    if (packet.method !== undefined) {
+      for (let req of wsmanager._rxQueue) {
+        if (req.id == packet.id) {
+          req.todelete = wsmanager.configuration.heartBeatInterval * 10;
+          if (req.answer !== null && wsmanager._state == 'online')
+            wsmanager._sock.send(req.answer);
+          return;
+        }
+      }
+      wsmanager._rxQueue.push({
+        'packet': e,
+        'id': packet.id,
+        'answer': null,
+        'todelete': wsmanager.configuration.heartBeatInterval * 10
+      });
+
+      console.log('wsmanager: rx packet', e.data);
+
+      packet.jsonrpc = null;
+
+      if (typeof (wsmanager._rxCallback) == 'function') {
+        wsmanager._rxCallback(packet)
+      }
     } else {
+      for (let i = 0; i < wsmanager._txQueue.length; i++) {
+        let req = wsmanager._txQueue[i];
+        if (req.id == packet.id) {
+          if (typeof (req.callback) == 'function') {
+            packet.id = null;
+            packet.jsonrpc = null;
+            req.callback(packet);
+            wsmanager._txQueue.splice(i, 1);
+            break;
+          }
+        }
+      }
+    }
+  },
+  'replyToPacket': function(id, answer) {
+    for (let req of wsmanager._rxQueue) {
+      if (req.id == id) {
+        req.answer = JSON.stringify(answer);
+        if (wsmanager._state == 'online') wsmanager._sock.send(req.answer);
+        return;
+      }
     }
   },
   '_onwsopen': function() {
@@ -81,10 +165,10 @@ var wsmanager = {
       wsmanager._statusChangedCallback(wsmanager._state);
   },
   '_onwsclose': function() {
-    console.log('wsmanager: disconnected');
-    wsmanager._stopHeartbeatTimer();
-    setTimeout(1000, wsmanager._connect());
     if (wsmanager._state !== 'offline') {
+      wsmanager._stopHeartbeatTimer();
+      setTimeout(wsmanager._connect(), 1000);
+      console.log('wsmanager: disconnected');
       wsmanager._state = 'offline';
       if (wsmanager._statusChangedCallback !== null)
         wsmanager._statusChangedCallback(wsmanager._state);
@@ -97,31 +181,23 @@ var wsmanager = {
     }
   },
   '_initHeartbeatTimer': function() {
+    if (wsmanager._heartbeatTimer !== null)
+      clearInterval(wsmanager._heartbeatTimer);
     wsmanager._heartbeatTimer = setInterval(
-        wsmanager.configuration.heartbeatinterval, wsmanager._sendHeartbeat);
+        wsmanager._sendHeartbeat,
+        wsmanager.configuration.heartBeatInterval * 2);
   },
   '_sendHeartbeat': function() {
-    if (wsmanager._sock !== null) {
-      if (wsmanager._currentTxPacket === null) {
-        wsmanager._sock.send(
-            JSON.stringify({'jsonrpc': '2.0', 'method': 'ping'}));
-        console.log('wsmanager: ping send');
-      } else {
-        wsmanager._trySend();
-        console.log('wsmanager: resend current packet');
-      }
+    if (wsmanager._state == 'online') {
+      wsmanager._sock.send(
+          JSON.stringify({'jsonrpc': '2.0', 'method': 'ping'}));
+      console.log('wsmanager: ping send');
     } else {
       wsmanager._onwsclose();
     }
   }
 
 };
-window.addEventListener('load', wsmanager.init);
 
 // можно добавить в статус попытки подключения, сделать интервал между попытками
 // как в вк - от 0.5с до 30с
-
-// при обрыве интернет-соединения сработает событие ws.close?
-
-// могут ли быть запоздалые пакеты? например, получили пакет с id == 5, потом
-// приходит с id == 4?
